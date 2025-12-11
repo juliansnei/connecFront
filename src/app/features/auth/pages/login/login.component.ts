0b1206abcf55cd1c98247b1de6ac5b38d821d501
julian <juliansnei@gmail.com>
2025-12-10 21:13:37 -0500

    feat:agregando talwind

--- src/app/features/auth/pages/login/login.component.ts
+++ src/app/features/auth/pages/login/login.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  loginForm!:FormGroup;
+
+  public constructor(private fb:FormBuilder){}
+
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+
+  initForm(){
+    this.loginForm = this.fb.group({
+      email:['',[Validators.required, Validators.email]],
+      password: ['',[Validators.required, Validators.minLength(8)]]
+    })
+  }
+
+  onSubmit(){
+    if(this.loginForm.invalid){
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+    console.log("Enviando al back");
+  }
 
 }
